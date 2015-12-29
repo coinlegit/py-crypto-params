77df4778fbc9b033256a5b1848289a90c028cc6e
Gian Luca Dalla Torre <gianluca.dallatorre@me.com>
2015-12-29 14:38:20 +0100

    Add missing module comment

--- cryptoparams.py
+++ cryptoparams.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
+"""
+    Python Cryptographic Function used to encode parameters for Web in a way that
+    `crypto-js <https://code.google.com/p/crypto-js/>`_ could decrypt it
+
+    .. moduleauthor:: Gian Luca Dalla Torre <gianluca@gestionaleauto.com>
+
+"""
+
 import binascii
 import Crypto.Cipher
 import Crypto.Hash.MD5
